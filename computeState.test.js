01485ca717ad930f0b8977074a97b0be9a6354ce
snitchjinx <jenny1000000@163.com>
2019-06-15 22:55:01 +0800

    [cz/ljy] fine-tuned test cases and comments

--- computeState.test.js
+++ computeState.test.js
@@ -2,51 +2,51 @@ let computeState = require('./computeState.js');
 let expect = require('chai').expect;
 
 describe("test of computeState function", function(){
-    it("dead(0)/alive(1) of a cell", function(){
+    it("dead cell with neighbor==3", function(){
         expect(computeState([[1,1,1],[0,0,0],[0,0,0]])).to.be.equal(1);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("alive cell with neighbor==3", function(){
         expect(computeState([[1,1,1],[0,1,0],[0,0,0]])).to.be.equal(1);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("dead cell with neighbor==2", function(){
         expect(computeState([[1,1,0],[0,0,0],[0,0,0]])).to.be.equal(0);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("alive cell with neighbor==2", function(){
         expect(computeState([[1,1,0],[0,1,0],[0,0,0]])).to.be.equal(1);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("dead cell with neighbor==2", function(){
         expect(computeState([[1,0,0],[0,0,0],[0,0,1]])).to.be.equal(0);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("alive cell with neighbor==2", function(){
         expect(computeState([[1,0,0],[0,1,0],[0,0,1]])).to.be.equal(1);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("dead cell with neighbor<2", function(){
         expect(computeState([[0,0,0],[0,0,1],[0,0,0]])).to.be.equal(0);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("alive cell with neighbor<2", function(){
         expect(computeState([[0,0,0],[0,1,1],[0,0,0]])).to.be.equal(0);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("dead cell with neighbor<2", function(){
         expect(computeState([[0,0,0],[0,0,0],[0,0,0]])).to.be.equal(0);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("alive cell with neighbor<2", function(){
         expect(computeState([[0,0,0],[0,1,0],[0,0,0]])).to.be.equal(0);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("dead cell with neighbor>3", function(){
         expect(computeState([[1,1,1],[1,0,1],[1,1,1]])).to.be.equal(0);
     });
 
-    it("dead(0)/alive(1) of a cell", function(){
+    it("alive cell with neighbor>3", function(){
         expect(computeState([[1,1,1],[1,1,1],[1,1,1]])).to.be.equal(0);
     });
 });
