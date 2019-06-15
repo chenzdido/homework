392e5a0ff666d29d0eb49265533a14a2f4a8ec7a
liujy <liujy@liujydeMacBook-Pro.local>
2019-06-15 17:56:51 +0800

    [cz/ljy] deepequal

--- padding.test.js
+++ padding.test.js
@@ -1,14 +1,16 @@
 let padding = require('./padding.js');
-let expect = require('chai').expect;
-var deepEqual = require('deep-equal');
+const deepEqualInAnyOrder = require('deep-equal-in-any-order');
+const chai = require('chai');
+chai.use(deepEqualInAnyOrder);
+const { expect } = chai;
 
 describe("test of padding of array", function(){
     it("3*3 array padded to 5*5", function(){
-        expect(padding([[1,1,1],[2,2,2],[3,3,3]])).to.be.deepEqual([[0,0,0,0,0],[0,1,1,1,0],[0,2,2,2,0],[0,3,3,3,0],[0,0,0,0,0]]);
+        expect(padding([[1,1,1],[2,2,2],[3,3,3]])).to.deep.equalInAnyOrder([[0,0,0,0,0],[0,1,1,1,0],[0,2,2,2,0],[0,3,3,3,0],[0,0,0,0,0]]);
+
     });
 
-    /*
     it("3*2 padded to 5*4", function(){
-        deepEqual(padding([[1,1],[2,2],[3,3]]), [[0,0,0,0],[0,1,1,0],[0,2,2,0],[0,3,3,0],[0,0,0,0]]);
-    });*/
+        expect(padding([[1,1],[2,2],[3,3]])).to.deep.equalInAnyOrder([[0,0,0,0],[0,1,1,0],[0,2,2,0],[0,3,3,0],[0,0,0,0]]);
+    });
 });
