a74b5b5ac8f6d3750f82597cf86c1c7c576dedd0
liujy <liujy@liujydeMacBook-Pro.local>
2019-06-15 15:08:17 +0800

    [ljy/cz] compute state test complete

--- computeState.test.js
+++ computeState.test.js
@@ -5,4 +5,48 @@ describe("test of computeState function", function(){
     it("dead(0)/alive(1) of a cell", function(){
         expect(computeState([[1,1,1],[0,0,0],[0,0,0]])).to.be.equal(1);
     });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[1,1,1],[0,1,0],[0,0,0]])).to.be.equal(1);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[1,1,0],[0,0,0],[0,0,0]])).to.be.equal(0);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[1,1,0],[0,1,0],[0,0,0]])).to.be.equal(1);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[1,0,0],[0,0,0],[0,0,1]])).to.be.equal(0);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[1,0,0],[0,1,0],[0,0,1]])).to.be.equal(1);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[0,0,0],[0,0,1],[0,0,0]])).to.be.equal(0);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[0,0,0],[0,1,1],[0,0,0]])).to.be.equal(0);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[0,0,0],[0,0,0],[0,0,0]])).to.be.equal(0);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[0,0,0],[0,1,0],[0,0,0]])).to.be.equal(0);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[1,1,1],[1,0,1],[1,1,1]])).to.be.equal(0);
+    });
+
+    it("dead(0)/alive(1) of a cell", function(){
+        expect(computeState([[1,1,1],[1,1,1],[1,1,1]])).to.be.equal(0);
+    });
 });
